027f32cc874a9a28f55a06a4db494da3c5fae7c3
gacaar <gabrielarao2013@gmail.com>
2019-05-15 17:58:36 +0000

    Revert "Desenvolvendo MACRO"
    This reverts commit f21a6667f3ce590681102111fcdbd8ed50d58289.

--- src/pre_processador.cpp
+++ src/pre_processador.cpp
@@ -3,22 +3,23 @@ Passagem zero do montador, retira espacos, tabs e quebras de linha desnecessario
 Processa as diretivas de pre-processamento EQU e IF e MACRO
 Recebe o arquivo <nome>.asm contendo o codigo do usuario.
 Gera um arquivo pre-processado <nome>.pre salvo em /out 
+Retorna um vetor que relaciona as linhas dos arquivos de entrada e saida.
 
-Erros detectados (somente relacionados com diretivas de pre processamento):
--- Nome de rotulo invalido
--- Rotulo nao definido (todo IF deve ter um EQU antes)
--- Rotulo repetido
--- Diretiva sem argumento
--- Diretiva com argumento invalido
+Erros detectados:
+-- Nome de rotulo invalido ok
+-- Rotulo nao definido (todo IF deve ter um EQU antes) ok
+-- Rotulo repetido ok
+-- Dois rotulos na mesma linha ok
+-- Diretiva sem argumento ok
+-- Diretiva com argumento invalido ok
 */
 
 #include "pre_processador.hpp"
-#include "utils.cpp"
 
 void equ(const string& label, const string& arg, list<TS>& tab, int line){
 	string nLabel = label.substr(0,label.size()-1);
-	if(arg == "/0" || !validConst(arg) || !validLabel(label)){ 
-		errLex(line); //argumento ausente ou argumento invalido ou label invalida
+	if(arg == "/0" || !validConst(arg)){ 
+		errLex(line);
 	}
 	else{
 		if(inList(nLabel,tab))
@@ -32,7 +33,7 @@ bool ifd(const string& label, list<TS>& tab, int line){
 	bool b;
 	TS simbol;
 	if(label == "\0" || !validLabel(label)){
-		errLex(line); //label ausente ou invalida
+		errLex(line);
 		b = true;
 	}else{
 		if(inList(label,tab,simbol)){
@@ -57,57 +58,32 @@ string takeFName(string fName){
 	}
 }
 
-void writeLine(string &labelAnt, const vector<string> &tokens, ofstream &fw, bool wMacro, list<MNDT> &macros){
-	if(!wMacro){
-		if(labelAnt != ""){
-			fw << labelAnt << " ";
-			labelAnt = "";
-		}
-		for(int t=0;t<tokens.size()-1;t++){
-			fw << tokens[t] << " ";
-		}
-		fw << endl;
-	}else{
-		std::string* def = &macros.back().def;
-		if(labelAnt != ""){
-			def->append(labelAnt+" ");
-			labelAnt = "";
-		}
-		for(int t=0;t<tokens.size()-1;t++){
-			def->append(tokens[t]+" ");
-		}
-		def->append("\n");		
-	}
+std::vector<int> preProc(string fileIn){
 
-}
-
-//void preProc(string fileIn){
-int main(){ string fileIn = "triangulo.asm";
-	bool wMacro = false; //Indica se esta escrevendo a definicao de uma MACRO
 	int lineCount = 0;
-	string line, labelEqu = "", nomeLabel = "", labelAnt = "", labelAntMacro = "";
+	string line, labelEqu = "", nomeLabel = "", labelAnt = "";
     list<TS> tabelaDeEqus;
 	list<TD> tabelaDirPre = inicializarTDPre();
-    vector<string> tokens;
-	list<MNDT> macros;
+    std::vector<string> tokens;
+	std::vector<int> lines;
 
-    ifstream fr(fileIn);
+    std::ifstream fr(fileIn);
 	string fileName = takeFName(fileIn);
-	ofstream fw(fileName+".pre");
+	std::ofstream fw(fileName+".pre");
     
-	while(getline(fr,line)){
+	while(std::getline(fr,line)){
 		lineCount++;
 		split(line,tokens);
 		if(tokens[0] != "\0"){
 			nomeLabel = "";
 			for(int t=0;t<tokens.size();t++){
 				if(isLabel(tokens[t])){
-					if(nomeLabel == "" && labelAnt == ""){ 
+					if(!validLabel(tokens[t]))
+						errLex(lineCount);
+					if(nomeLabel != "") 
+						errSin(lineCount);
+					else
 						nomeLabel = (tokens[t]);
-					}else{ //+ de 1 label na linha: ignora, escreve a linha e vai pra proxima
-						writeLine(labelAnt, tokens, fw, wMacro, macros);
-						break;
-					}
 				}
 				else if(inList(upCase(tokens[t]),tabelaDirPre)){
 					if(upCase(tokens[t]) == "EQU"){
@@ -118,7 +94,7 @@ int main(){ string fileIn = "triangulo.asm";
 								labelEqu = labelAnt;
 								labelAnt = "";
 							}else{
-								errSin(lineCount); //label ausente
+								errSin(lineCount);
 							}
 						if(labelEqu != ""){
 							equ(upCase(labelEqu), tokens[t+1], tabelaDeEqus, lineCount);
@@ -126,7 +102,7 @@ int main(){ string fileIn = "triangulo.asm";
 						}
 					}else{
 						if(!ifd(upCase(tokens[t+1]), tabelaDeEqus, lineCount)){
-							getline(fr,line);
+							std::getline(fr,line);
 							lineCount++;
 						}
 					}
@@ -136,34 +112,19 @@ int main(){ string fileIn = "triangulo.asm";
 					if(nomeLabel != "\0") labelAnt=nomeLabel;
 				}
 				else{
-					writeLine(labelAnt, tokens, fw, wMacro, macros);
+					if(labelAnt != ""){
+						fw << labelAnt << " ";
+						labelAnt = "";
+					}
+					for(int t=0;t<tokens.size()-1;t++){
+						fw << tokens[t] << " ";
+					}
+					fw << endl;
+					lines.push_back(lineCount);
 					break;
 				}
 			}
 		}
 	}
 }
-/* 
-	Ler a linha, incrementar counter e separar
-    nomeLabel = "";
-	Procurar Labels ou diretivas de pre-processamento na lista
-        Se elemento 0 for NULL, proxima linha
-        Se achou label, verifica validade, verifica se nomeLabel = "" e guarda em nomeLabel
-        Se achou diretiva (nao le mais essa linha):
-            Se EQU, verifica se nomeLabel != "" (se for, verifica labelAnt), verifica se existe argumento e se eh valido
-                Procura label L na tabela e, se nao achar, salvar
-                nomeLabel = "\0" (a label foi usada) 
-			SE IF, verifica se existe argumento e eh valido, verifica na tab, incrementa e pula se true	
-			SE MACRO, verifica nome valido e num de arg
-				Salvar nome e numero de variaveis na MNDT, ativar wMacro. Se labelAnt!="" guarda em labelAntMacro
-			SE END, verifica se esta em modo macro
-				Desativa wMacro e recupera labelAntMacro se !=""
-		Se fim da linha
-			Se nomeLabel != "\0" -> labelAnt = nomeLabel
-		Se nao
-			Verifica se ha labelAnt nao usada (perde a linha que a label apareceu, mas como ja eh 
-				verificado se ha erro na label aqui entao nao importa mais)
-			Copia linha, salva o numero da linha e prox linha
-			
-	*/
 
--- src/pre_processador.hpp
+++ src/pre_processador.hpp
@@ -6,13 +6,6 @@
 #include <string>
 #include "utils.hpp"
 
-//Nome, numero de argumentos e definicao da macro
-typedef struct MNDT{
-    string nome;
-    int arg;
-    std::string def;
-} MNDT;
-
 void equ(const string& label, const string& arg, list<TS>& tab, int line);
 
 //se false, pula prox linha. Em caso de erro retorna true
@@ -20,6 +13,6 @@ bool ifd(const string& label, list<TS>& tab, int line);
 
 string takeFName(string fName);
 
-void writeLine(string &labelAnt, const vector<string> &tokens, ofstream &fw);
+std::vector<int> preProc(string fileIn);
 
 #endif
